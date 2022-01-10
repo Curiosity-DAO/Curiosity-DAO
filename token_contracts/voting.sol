762dadb84ff43949042aaf780ed3f80fa26b628c
theLucasWalters <lwalters224@gmail.com>
2022-01-09 21:14:34 -0700

    Add voting.sol

---
+++ token_contracts/voting.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+/**
+ * @dev For details, read the README.md file in the Github repo @ https://github.com/Curiosity-DAO/Curiosity-DAO/blob/main/README.md
+ */
+
+// Setup
+import "./curiosity.sol";
+
+/**
+ * @title Curiosity
+ * @author Lucas Walters - https://www.github.com/theLucasWalters
+ */
+contract CuriosityVoting is Curiosity {
+    // voting contract
+}
